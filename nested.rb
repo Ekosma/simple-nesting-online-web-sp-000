d4130a48fcec42eca128b633c6361a9ea6676929
Evelyn Kosma <ezranaja@gmail.com>
2021-03-03 18:17:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,8 +105,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for] = "GUI"
-  puts programmer_hash
+  changing_alan[:alan_kay][:known_for] = "GUI"
 end
 
     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
